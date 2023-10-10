0e773d5c3cbf0d2fa2c4bece3b8253ab23ad610f
Crowdbotics <team@crowdbotics.com>
2023-10-10 19:55:52 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "resell-and-tell-43862",
+  url: "https://resell-and-tell-43862.botics.co",
+  api: "https://resell-and-tell-43862.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
